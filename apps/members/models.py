852da2186bbe31206d435172c50a12eb812192fe
VICTORMUHIRE <muhirevictor2020@gmail.com>
2026-01-28 11:55:31 +0200

    modification du model membre

--- apps/members/models.py
+++ apps/members/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 class Member(models.Model):
-    name = models.CharField(max_length=100) 
-    post_name = models.CharField(max_length=100, blank=True) 
-    phone = models.CharField(max_length=20, unique=True) 
-    address = models.TextField() 
+    TYPE_CHOICES = [
+        ('PARTICULIER', 'Particulier'),
+        ('ENTREPRISE', 'Entreprise/Organisation'),
+    ]
+
+    full_name = models.CharField(max_length=255, verbose_name="Nom Complet / Raison Sociale")
+    client_type = models.CharField(max_length=20, choices=TYPE_CHOICES, default='PARTICULIER')
+    phone = models.CharField(max_length=20, unique=True, verbose_name="Téléphone (+243...)")
+    address = models.TextField(blank=True, null=True, verbose_name="Adresse Physique")
+    id_card_number = models.CharField(max_length=50, blank=True, null=True, verbose_name="N° Pièce d'Identité")
     created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.name} {self.post_name}"
+        return f"{self.full_name} "
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = "Membre"
